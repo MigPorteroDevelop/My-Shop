2731d5e2b9c02afb2d09031a23ed2a3655a65f0c
Miguel <migporuiz@gmail.com>
2023-08-23 12:48:26 +0200

    Stock control

--- src/components/Basket.vue
+++ src/components/Basket.vue
@@ -19,29 +19,29 @@ const decrement = (id) => {
     </div>
     <div class="m-auto grid grid-cols-1">
       <div v-for="product in store.productsCart">
-        {{ product }}
-        <!-- 
-          comento->
+        <div v-if="product">
           <div id="product-card" class="p-4 border-2">
-          <div id="product-image" class="w-40 h-40">
-            <img :src="product.photo">
-          </div>
-          <div id="product-info" class="text-center my-5">
-            <h5 class="font-bold mb-2">{{ product.name }}</h5>
-            <h6>{{ product.price + " €" }}</h6>
-          </div>
-          <div class="flex items-center">
-            <div class="m-auto flex">
-              <button @click="decrement(product.id)"
-                class="border-2 border-black rounded-full w-6 h-6 flex items-center justify-center">-</button>
-              <div class="px-4">
-                <input type="number" v-model="store.productsCart[product.id].items" class="border rounded w-12 text-center" />
-              </div>
-              <button @click="increment(product.id)"
-                class="border-2 border-black rounded-full w-6 h-6 flex items-center justify-center">+</button>
+            <div id="product-image" class="w-40 h-40">
+              <img :src="product.photo">
             </div>
-          </div> 
-        </div> -->
+            <div id="product-info" class="text-center my-5">
+              <h5 class="font-bold mb-2">{{ product.name }}</h5>
+              <h6>{{ product.price + " €" }}</h6>
+            </div>
+            <div class="flex items-center">
+              <div class="m-auto flex">
+                <button @click="decrement(product.id)"
+                  class="border-2 border-black rounded-full w-6 h-6 flex items-center justify-center">-</button>
+                <div class="px-4">
+                  <input type="number" v-model="store.productsCart[product.id].items"
+                    class="border rounded w-12 text-center" />
+                </div>
+                <button @click="increment(product.id)"
+                  class="border-2 border-black rounded-full w-6 h-6 flex items-center justify-center">+</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <!--<div>
         <p> {{ productCart.totalPrice + " €" }}</p>
--- src/stores/shopProducts.js
+++ src/stores/shopProducts.js
@@ -1,40 +1,42 @@
 import { defineStore } from 'pinia';
 import productsBase from '@/assets/productsBase.json';
 
-//const totalPrice += this.productsCart[id].items.price;
-// 
 export const useShopProducts = defineStore('shopProducts', {
 
   state: () => ({
-    
     productsCart: []
   }),
   actions: {
+    //Aquí comprobamos que la posición del producto en el array. 
+    //Si coincide, devolvemos true.
     getProductById(id) {
-      for( let i in productsBase.products ) {
-        if(productsBase.products[i].id == id) {
+      for (let i in productsBase.products) {
+        if (productsBase.products[i].id == id) {
           return productsBase.products[i];
         }
       }
       return false;
     },
     increment(id) {
-      // Si existe un producto, y los items del carrito son menores o igual al stock
-      if (this.productsCart[id] && this.productsCart[id].items <= productsBase.products.stock) {
+      //Si existe un producto y los items del carrito son menores o igual al stock.
+      //Utilizamos la función "getProductById" para tener el id exacto y accedemos a su stock.
+      if (this.productsCart[id] && this.productsCart[id].items < this.getProductById(id).stock) {
         //Se añade un item más
         this.productsCart[id].items++;
+
         // Se guarda en la variable la suma del precio de los items
-        this.totalPrice += this.productsCart[id].items.price;
-        //Si existe un producto, y el numero de items es igual al sotck del producto
-      } if (this.productsCart[id] && this.productsCart[id].items == productsBase.products.stock) {
+        //this.totalPrice += this.productsCart[id].items.price;
+
+        //Si existe un producto, y el numero de items es igual al stock del producto
+      } else if (this.productsCart[id] && this.productsCart[id].items == this.getProductById(id).stock) {
         alert("No hay mas stock disponible.")
 
       } else {
-        // Si el producto no esta en el carrito -> Crea el id del producto en el array 
+        // Si el producto no esta en el carrito crea el id del producto en el array 
+        // y asigna el id dentro del array al id de producto 
+        this.productsCart[id] = this.getProductById(id);
 
-
-        this.productsCart[id] = this.getProductById( id );        // Añade un item del producto
-        console.log(this.productsCart[id])
+        // Añade un item del producto
         this.productsCart[id].items = 1;
       }
     },
@@ -45,7 +47,7 @@ export const useShopProducts = defineStore('shopProducts', {
         this.productsCart[id].items--;
 
         // Se guarda en la variable la resta del precio de los items
-        this.totalPrice -= this.productsCart[id].items.price;
+        //this.totalPrice -= this.productsCart[id].items.price;
 
         // Si la cantidad de items de un producto llega a 0
         if (this.productsCart[id].items === 0) {
@@ -53,7 +55,6 @@ export const useShopProducts = defineStore('shopProducts', {
           delete this.productsCart[id];
         }
       }
-    },
-    
+    }
   }
 });
